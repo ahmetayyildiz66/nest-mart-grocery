3fb779779edf65f8722fdbdef31e64642f48a029
ahmet <ahmet.ayyildiz@qlik.com>
2023-04-03 15:25:19 +0200

    feat: add product cards

---
+++ src/components/PopularProductCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="w-[280px] h-[400px] border rounded-xl p-5 pt-0 shadow-md flex flex-col justify-center">
+    <img :src="images[0]" :alt="title" class="object-fit mt-14 h-[200px] mx-auto" />
+    <h4 class="text-sm font-bold leading-5 mt-5 h-10">{{ title }}</h4>
+    <p class="text-gray-450 text-sm mt-3">{{ weight }} gram</p>
+    <p class="mt-3">
+      <span class="text-green-750 font-bold text-xl">${{ price }}</span>
+      <span class="text-gray-450 text-sm ml-1 line-through" v-if="price !== discountPrice">${{ discountPrice.toFixed(2) }}</span>
+    </p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  title: string
+  price: number
+  discountPrice: number
+  weight: number
+  rating: number
+  images: string[]
+}
+
+const props = defineProps<Props>()
+</script>
--- src/components/PopularProducts.vue
+++ src/components/PopularProducts.vue
@@ -1,10 +1,47 @@
 <template>
-  <section class="mx-40 mt-16 flex items-center justify-between">
-    <h2 class="text-3xl font-semibold">Popular Products</h2>
-    <PopularFilterList />
-  </section>
+  <div class="mx-40 mt-16">
+    <section class="flex items-center justify-between">
+      <h2 class="text-3xl font-semibold">Popular Products</h2>
+      <PopularFilterList />
+    </section>
+    <span v-if="loading">Loading...</span>
+    <div class="mt-12 grid grid-cols-5 gap-6" v-if="!loading">
+      <PopularProductCard
+        v-for="res in result.popularProducts" :key="res.id"
+        :title="res.title"
+        :price="res.price" 
+        :rating="res.rating"
+        :discountPrice="res.discountPrice"
+        :weight="res.weight"
+        :images="res.images"
+      />
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import PopularFilterList from './PopularFilterList.vue';
+import gql from 'graphql-tag'
+import { useQuery } from '@vue/apollo-composable'
+
+import PopularFilterList from './PopularFilterList.vue'
+import PopularProductCard from './PopularProductCard.vue'
+
+const POPULAR_PRODUCTS = gql`
+  query PopularProducts {
+    popularProducts {
+      id
+      title
+      price
+      rating
+      discountPrice
+      weight
+      category
+      images
+    }
+  }
+`
+
+const { result, loading, error } = useQuery(POPULAR_PRODUCTS)
+
+console.log('popular products result: ', result)
 </script>
